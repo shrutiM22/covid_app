ff9f1aa4104841e01586eb3467c6e2e990b3cb85
shrutiM22 <69834639+shrutiM22@users.noreply.github.com>
2022-03-23 18:18:32 +0530

    Update Controller.java

--- covid_info_app/src/main/Java/Controller.java
+++ covid_info_app/src/main/Java/Controller.java
@@ -1,6 +1,5 @@
 package main.Java;
 
-//import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -15,20 +14,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-	@RequestMapping("/submit")
+	//@RequestMapping("/submit")
 	public ModelAndView covidData(HttpServletRequest request,HttpServletResponse response) throws ClassNotFoundException, SQLException {
 		ModelAndView mv=new ModelAndView();
 		mv.setViewName("display.jsp");
 
-		Class.forName("org.postgresql.Driver");
-		Connection conn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","test");
-		Statement stmt= conn.createStatement();
-		ResultSet r=stmt.executeQuery("SELECT location, Count(total_cases) FROM covid_data WHERE new_cases> 1000 GROUP BY location,total_cases ORDER BY total_cases;");
 		
-		while(r.next()) {
-		mv.addObject("location",r.getString(1));
-		mv.addObject("cases",r.getString(2));
-		}
 		return mv;
 	
 }
